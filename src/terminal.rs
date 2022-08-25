2abd6e275f291feec077ecaf0c094a6d13bd14b7
Shadorain <jordanf7517@gmail.com>
2022-08-24 23:29:04 -0400

    Updated terminal.rs, some documentation and getting ready for rehaul

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,6 @@
+use crossterm::{cursor as c, event as e, execute, queue, style as s, terminal as t, ErrorKind};
+use std::io::{self, stdout, Stdout, Write};
 use thiserror::Error;
-use std::io::{ Stdout, stdout, self, Write };
-use crossterm::{ cursor as c, execute, queue, terminal as t, style as s,
-    event as e, ErrorKind };
 
 #[derive(Error, Debug)]
 pub enum TermError {
@@ -31,10 +30,11 @@ impl From<Rgb> for s::Color {
 }
 
 /// Event is a wrapper around crossterm's Event enum
-/// Note: Resize taken out because it should be handled immediately
+/// NOTE: Resize taken out because it should be handled immediately
 pub enum Event {
     Key(e::KeyEvent),
     Mouse(e::MouseEvent),
+    Paste(String),
     /* Resize(crossterm::event::Resize), */
 }
 
@@ -55,7 +55,10 @@ impl Terminal {
     pub fn new() -> Result<Self> {
         let size = t::size()?;
         Ok(Self {
-            size: Size { cols: size.0, rows: size.1 },
+            size: Size {
+                cols: size.0,
+                rows: size.1,
+            },
             _stdout: stdout(),
         })
     }
@@ -64,9 +67,6 @@ impl Terminal {
         Ok(self._stdout.flush()?)
     }
 
-
-    /* Read ----------------------------------------------------------------- */
-
     /// Reads and returns an Event from the terminal
     /// Handles resize events automatically
     pub fn read_event(&mut self) -> Result<Event> {
@@ -74,111 +74,91 @@ impl Terminal {
             match e::read()? {
                 e::Event::Key(key) => return Ok(Event::Key(key)),
                 e::Event::Mouse(mouse) => return Ok(Event::Mouse(mouse)),
-                e::Event::Resize(x, y) => {
-                    self.size = Size { cols: x, rows: y }
-                },
+                e::Event::Resize(x, y) => self.size = Size { cols: x, rows: y },
+                e::Event::Paste(s) => return Ok(Event::Paste(s)),
                 _ => todo!(),
             }
         }
     }
 
-    /* Cursor --------------------------------------------------------------- */
-
+    /// Queues a cursor hide command
     pub fn cursor_hide(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            c::Hide
-        )?;
+        queue!(self._stdout, c::Hide)?;
         Ok(self)
     }
+    /// Queues a cursor show command
     pub fn cursor_show(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            c::Show
-        )?;
+        queue!(self._stdout, c::Show)?;
         Ok(self)
     }
 
+    /// Queue cursor position to be updated
+    ///
+    /// * `col`: column position
+    /// * `row`: row position
     pub fn cursor_position(&mut self, col: u16, row: u16) -> Result<&mut Self> {
-        queue!(self._stdout,
-            c::MoveTo(col, row)
-        )?;
+        queue!(self._stdout, c::MoveTo(col, row))?;
         Ok(self)
     }
 
-
-    /* Display -------------------------------------------------------------- */
-
+    /// Queues the screen to be cleared
     pub fn clear_screen(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            t::Clear(t::ClearType::All)
-        )?;
+        queue!(self._stdout, t::Clear(t::ClearType::All))?;
         Ok(self)
     }
+    /// Queues the current line to be cleared
     pub fn clear_line(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            t::Clear(t::ClearType::CurrentLine)
-        )?;
+        queue!(self._stdout, t::Clear(t::ClearType::CurrentLine))?;
         Ok(self)
     }
-
-
-    /* Coloring ------------------------------------------------------------- */
 
     pub fn fg_set(&mut self, rgb: Rgb) -> Result<&mut Self> {
-        queue!(self._stdout,
-            s::SetForegroundColor(rgb.into())
-        )?;
+        queue!(self._stdout, s::SetForegroundColor(rgb.into()))?;
         Ok(self)
     }
     pub fn bg_set(&mut self, rgb: Rgb) -> Result<&mut Self> {
-        queue!(self._stdout,
-            s::SetBackgroundColor(rgb.into())
-        )?;
+        queue!(self._stdout, s::SetBackgroundColor(rgb.into()))?;
         Ok(self)
     }
     pub fn fg_reset(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            s::SetForegroundColor(s::Color::Reset)
-        )?;
+        queue!(self._stdout, s::SetForegroundColor(s::Color::Reset))?;
         Ok(self)
     }
     pub fn bg_reset(&mut self) -> Result<&mut Self> {
-        queue!(self._stdout,
-            s::SetBackgroundColor(s::Color::Reset)
-        )?;
+        queue!(self._stdout, s::SetBackgroundColor(s::Color::Reset))?;
         Ok(self)
     }
-
-
-    /* Window Management ---------------------------------------------------- */
 
     pub fn size_get(&self) -> &Size {
         &self.size
     }
     pub fn size_set(&mut self, size: Size) -> Result<()> {
         self.size = size;
-        Ok(queue!(self._stdout,
+        Ok(queue!(
+            self._stdout,
             t::SetSize(self.size.cols, self.size.rows)
         )?)
     }
 
-
-    /* Init / Deinit -------------------------------------------------------- */
-
     pub fn startup(&mut self) -> Result<()> {
-        execute!(self._stdout,
+        execute!(
+            self._stdout,
             t::EnterAlternateScreen,
             s::ResetColor,
             t::Clear(t::ClearType::All),
             c::MoveTo(0, 0),
-        ).map_err(|_| TermError::Startup)?;
+        )
+        .map_err(|_| TermError::Startup)?;
 
         t::enable_raw_mode().map_err(|_| TermError::Startup)
     }
     pub fn cleanup(&mut self) -> Result<()> {
-        execute!(self._stdout,
+        execute!(
+            self._stdout,
             s::ResetColor,
             c::Show,
             t::LeaveAlternateScreen
-        ).map_err(|_| TermError::Cleanup)
+        )
+        .map_err(|_| TermError::Cleanup)
     }
 }
