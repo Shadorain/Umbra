efc7277e34f48e8737f375c597c5ad7e06402937
Shadorain <jordanf7517@gmail.com>
2022-09-09 00:17:25 -0400

    Updated Document example to utilize the backend

--- src/bin/document.rs
+++ src/bin/document.rs
@@ -1,18 +1,24 @@
-use umbra::{IEvent, Umbra};
+use umbra::{IEvent, Umbra, UError, Key, KeyModifiers, BackendSetter};
+
+// struct Document {
+//     title: &str,
+// }
 
 /// Will be a simple example of rendering a Document with Umbra
-fn main() {
-    // let mut umbra = Umbra::new().config(serialized_data);
-
-    let mut umbra: Umbra = Umbra::new().expect("Umbra error: {0}");
-    umbra.init().expect("Panic... umbra init failed");
+fn main() -> Result<(), UError> {
+    let mut umbra: Umbra = Umbra::new();//.config(serialized_data);
+    umbra.set_crossterm()?;
 
     loop {
-        match umbra.read_event().unwrap() {
-            IEvent::Key(key) => print!("Key {:?}", key),
+        match umbra.read_event()? {
+            IEvent::Key(modif, key) => if key == Key::Char('q') && modif == KeyModifiers::CONTROL { break; },
+            IEvent::Mouse(m) => println!("Mouse ptr: ({0}, {1})", m.x, m.y),
+            IEvent::Resize(r) => println!("Screen has been resized to: ({0}, {1})", r.x, r.y),
             IEvent::Paste(s) => print!("Paste {0}", s),
             IEvent::FocusGained => print!("Window gained focus"),
             IEvent::FocusLost => print!("Window lost focus"),
         }
     }
+
+    Ok(())
 }
