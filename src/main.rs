02b9b71c8e7c96db4f935c173010f61e40efed32
Shadorain <jordanf7517@gmail.com>
2023-04-29 22:14:32 -0400

    scratch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{stdout, BufWriter, Stdout};
+
+use crossterm::{event as e, queue, style as s, terminal as t, Result};
+
+pub struct Terminal {
+    buffer: BufWriter<Stdout>,
+}
+
+impl Terminal {
+    pub fn new() -> Self {
+        Terminal {
+            buffer: BufWriter::new(stdout()),
+        }
+    }
+    fn init(&mut self) -> Result<()> {
+        queue!(
+            self.buffer,
+            t::EnterAlternateScreen,
+            s::ResetColor,
+            t::Clear(t::ClearType::All),
+            e::EnableMouseCapture,
+            e::EnableFocusChange,
+        )?;
+        t::enable_raw_mode()?;
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    while true {
+        Terminal::new().init();
+    }
 }
